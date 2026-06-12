4d8a2300feb3e2b6e839d504678bb22c65bbdf1e
breaze <simon.gaviriao@gmail.com>
2026-06-12 14:47:02 -0500

    Agregando la clase Busqueda con la implementación de la busqueda lineal

---
+++ src/main/java/org/breaze/Busqueda.java
@@ -0,0 +1,14 @@
+package org.breaze;
+
+public class Busqueda {
+
+    public int buscar(int[] lista, int valor) {
+        for (int i = 0; i < lista.length; i++) {
+            if (lista[i] == valor) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
